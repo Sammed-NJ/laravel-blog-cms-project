03a612ebb54459f7062f879dcfb2247b3e38bb01
Sammed NJ <73836280+Sammed-NJ@users.noreply.github.com>
2023-01-12 16:33:52 +0530

    Reorder the Code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Post;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,8 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(10)->create()->each(function ($user) {
-            $user->post()->save(Post::factory()->make());
-        });
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,16 +13,6 @@ use App\Http\Controllers\AdminController;
 |
 */
 
-
-
-Auth::routes();
-
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/post/{post}', [PostController::class, 'show'])->name('post');
-
-// Route::middleware('auth')->group(function () {
-Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
-
-Route::get('/admin/post/create', [PostController::class, 'create'])->name('post.create');
-Route::post('/admin/post', [PostController::class, 'store'])->name('post.store');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
